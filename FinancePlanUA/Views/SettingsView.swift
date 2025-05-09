62ad259eebaa88a9084882144c1127c4be8b1b7e
vtodorchuk <bozodee8@gmail.com>
2025-05-09 22:02:13 +0300

    Add icon, settings and onTapGesture to view Allocation Details

---
+++ FinancePlanUA/Views/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  FinancePlanUA
+//
+//  Created by Vlady Todorchuk on 09.05.2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
